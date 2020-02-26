66d7841ef1427523a605bbab3899a6d6b79728f0
LmPladano <pladanoc@icloud.com>
2020-02-26 10:20:00 -0600

    auth 0 fully implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./components/NavBar";
 import { useAuth0 } from "./react-auth0-spa";
 
 function App() {
   const { loading } = useAuth0();
+   
+
 
   if (loading) {
     return <div>Loading...</div>;
