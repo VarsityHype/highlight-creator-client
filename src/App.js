0dbd466013f33a1c58e11bab09bad606be83f93e
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-02-26 15:54:17 -0600

    created video upload component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 // src/App.js
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import { useAuth0 } from "./react-auth0-spa";
+import UploadVideo from './components/UploadVideo'
 
 function App() {
   const { loading } = useAuth0();
-   
-
 
   if (loading) {
     return <div>Loading...</div>;
@@ -18,6 +17,7 @@ function App() {
       <header>
         <NavBar />
       </header>
+      <UploadVideo />
     </div>
   );
 }
