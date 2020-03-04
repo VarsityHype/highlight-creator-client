3bf81b98ffa98382c71c272416487f9badce62dc
Rawand Dargali <rdargali@yahoo.com>
2020-03-04 15:14:35 -0600

    footer positioned

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../css/footer.css";
+import "../css/Footer.css";
 
 import { fade, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
