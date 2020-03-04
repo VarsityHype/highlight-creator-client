c843cdc2236a1cd396311828578ac02778f0534a
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-03-04 15:41:31 -0600

    user can create a playlist

--- src/components/PlaylistsMenu.js
+++ src/components/PlaylistsMenu.js
@@ -1,9 +1,26 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 import {connect} from 'react-redux'
 
 function PlaylistsMenu() {
 
+//clip id, user id, playlist id
+
+    const [newPlaylist, setNewPlaylist] = useState({})
+
+    const handleChange = (e) => {
+        setNewPlaylist({
+            ...newPlaylist,
+            [e.target.name]: e.target.value
+        })
+    }
+
     const createPlaylists = () => {
+        const url = 'http://localhost:3001/playlists/create-playlist'
+        axios.post(url, {
+            newPlaylist
+        })
+
 
     }
 
@@ -30,6 +47,8 @@ function PlaylistsMenu() {
     return (<>
     
         <h1>Playlists</h1>
+        <input name="title" placeholder="playlist title" onChange={handleChange} />
+        <input name="description" placeholder="playlist description" onChange={handleChange} />
         <button onClick={() => createPlaylists()}>Create a playlist</button>
 
     </>)
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,8 +1,32 @@
 import React, {useEffect, useState} from 'react'
 import {connect} from 'react-redux'
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
 import axios from 'axios'
 
 function Video(props) {
+
+    const [playlistData, setPlaylistData] = useState({})
+
+    useEffect(() => {
+        const url = ''
+        fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            const playlists = json.map((playlist) => {
+
+
+                return (<>
+                
+                    
+                
+                </>)
+            })
+            setPlaylistData(playlists)
+        })
+    }, [])
 
     const getUrlAndSeeVideo = (videoUrl, videoTitle) => {
         props.getUrl(videoUrl, videoTitle)
@@ -12,7 +36,7 @@ function Video(props) {
     const addToPlaylist = () => {
         const url = ''    
         axios.post(url, {
-            
+
         })
     }
 
@@ -49,7 +73,15 @@ function Video(props) {
                                 <div className="h1-title-div">
                                     <h1 onClick={() => getUrlAndSeeVideo(videoUrl, videoTitle)}>{videoTitle}</h1>
                                 </div>
-                                <button onClick={() => addToPlaylist()}>Add to playlist</button>
+                                <FormControl>
+                                    <InputLabel id="add-to-playlist">Add to playlist</InputLabel>
+                                    <Select labelId="add-to-playlist">
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                    </Select>
+                                </FormControl>
+
                                 <p>{video.description}</p>
                             </div>
                         </div>
